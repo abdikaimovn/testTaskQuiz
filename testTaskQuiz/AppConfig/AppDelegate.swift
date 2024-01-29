8a538a507e19decd282b202f3ea498ea7b3d4ab7
Nurdaulet <abdikaimovnurdaulet@gmail.com>
2024-01-29 22:21:31 +0600

    Basic settings, added snapkit

---
+++ testTaskQuiz/AppConfig/AppDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.swift
+//  testTaskQuiz
+//
+//  Created by Нурдаулет on 29.01.2024.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+}
+
